e7a8dbc81470f1aba6c2ec6fb9f927b659897b9a
ferdinands <ferdinand.strobel@infrontfinance.com>
2022-10-20 15:54:48 +0200

    Add bottom-Bar, impressum is missing

---
+++ src/app/comps/buttom-section/buttom-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buttom-section',
+  templateUrl: './buttom-section.component.html',
+  styleUrls: ['./buttom-section.component.css']
+})
+export class ButtomSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
