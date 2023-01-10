0c4f287e1b614d80ec45bdb2570a5ec94fcfc2a8
ferdinands <ferdinand.strobel@infrontfinance.com>
2023-01-10 16:30:44 +0100

    Updated Mobile View

---
+++ src/app/compsHandy/handy-button-section/handy-button-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-handy-button-section',
+  templateUrl: './handy-button-section.component.html',
+  styleUrls: ['./handy-button-section.component.css']
+})
+export class HandyButtonSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
