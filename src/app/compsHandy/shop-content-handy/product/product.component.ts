8be1a5f70ea76d014b5dedb95a82bd4eecc86afe
ferdinands <ferdinand.strobel@infrontfinance.com>
2023-01-12 16:52:16 +0100

    Add first view of Shop for Mobile

--- src/app/compsHandy/shop-content-handy/product/product.component.ts
+++ src/app/compsHandy/shop-content-handy/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-product',
@@ -6,6 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  selected:number  = 0;
+
+  @Input()
+  title:string  = "";
+
+
+  @Input()
+  price:string = "";
+
+
+  @Input()
+  products:string[]   =  [] ;
+
+
 
   constructor() { }
 
